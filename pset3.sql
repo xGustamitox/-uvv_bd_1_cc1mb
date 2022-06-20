d28aaa5326383ec110a520b5f25bec26c4a54508
xGustamitox <103529876+xGustamitox@users.noreply.github.com>
2022-06-20 17:34:14 -0300

    Update pset3.sql

--- pset3.sql
+++ pset3.sql
@@ -1,4 +1,15 @@
-WITH recursive classificacao_P AS (
-SELECT cd.codigo, concat(nome) AS nome, cd.codigo_pai
-FROM  classificacao AS cd
-WHERE cd.codigo_pai is null
+WITH recursive classificacao_P 
+AS ( SELECT cod_p.codigo, concat(nome) AS nome, cod_p.codigo_pai
+FROM  classificacao AS cod_p
+WHERE cod_p.codigo_pai is null
+  
+UNION ALL
+
+SELECT cod_f.codigo, concat(cla.nome,|| '-->' ||, cod_f.nome), cod_f.codigo_pai
+FROM classificacao AS cod_f
+INNER JOIN classificacao_P AS cla ON cla.codigo = cod_f.codigo_pai
+WHERE cod_f.codigo_pai IS NOT NULL)
+
+SELECT*
+FROM classificacao_P
+ORDER BY classificacao_P.nome;  
